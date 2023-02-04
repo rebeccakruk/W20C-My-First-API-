ea46f63dd44d0b7be648a67837f3985e1dd84ca2
rk <rebeccakruk@outlook.com>
2023-02-03 17:43:44 -0700

    get and post working

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
 from flask import Flask
+import json
+from dbhelpers import run_statement
+
+app = Flask(__name__)
+
+@app.get('/animals')
+def get_animals():
+    result = run_statement("CALL list_animals()")
+    if (type(result) == list):
+        result_json = json.dumps(result, default=str)
+        animals = json.loads(result_json)
+        return animals
+    else:
+        return 'Close, but no cigar'
+    
+@app.post('/animals')
+def post_animal():
+    new_animal = 'raccoon'
+    result = run_statement("CALL post_animal()")
+    if (type(result) == list):
+        result_json = json.dumps(result, default=str)
+        new_animal = json.loads(result_json)
+        return "You've added: ", new_animal, "to the list!"
+    else:
+        return 'Your animal was not added to the list'
+
+app.run(debug = True)
