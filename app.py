acdcbae6873ee022df5dbabd37003686029d6bfa
rk <rebeccakruk@outlook.com>
2023-02-07 08:39:41 -0700

    all statements working properly

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 import json
-from dbhelpers import run_statement
+from dbhelpers import run_statement, execute_statement, connect_db
 
 app = Flask(__name__)
 
-@app.get('/animals')
+@app.get('/api/animals')
 def get_animals():
     result = run_statement("CALL list_animals()")
     if (type(result) == list):
@@ -12,17 +12,36 @@ def get_animals():
         animals = json.loads(result_json)
         return animals
     else:
-        return 'Close, but no cigar'
-    
-@app.post('/animals')
+        return "Something went wrong, please try again."
+
+@app.post('/api/animals_new')
 def post_animal():
-    new_animal = 'raccoon'
-    result = run_statement("CALL post_animal()")
-    if (type(result) == list):
-        result_json = json.dumps(result, default=str)
-        new_animal = json.loads(result_json)
-        return "You've added: ", new_animal, "to the list!"
+    new_animal = 'wolf'
+    result = run_statement("CALL post_animal(?)", [new_animal])
+    if (new_animal == None):
+        return "You must specify a new animal"
+    if result == None:
+        return "You've successfully added a new animal: {}".format(new_animal)
+
+@app.patch('/api/animals_update')
+def update_animal():
+    animal_change = 'coyote'
+    if (animal_change == None):
+        return "Please enter a valid name"
+    result = run_statement("CALL update_animal(?)", [animal_change])
+    if result == None:
+        return "You've successfully updated the animal to: {}".format(animal_change)
     else:
-        return 'Your animal was not added to the list'
+        return "Something went wrong, please try again."
+
+@app.delete('/api/animals_delete')
+def delete_animal():
+    animal_delete = 'cat'
+    if (animal_delete == None):
+        return "Please enter a valid name"
+    result = run_statement("CALL delete_animal(?)", [animal_delete])
+    if result == None:
+        return "You've successfully removed {} from the list".format(animal_delete)
+    else: "Something went wrong, please try again"
 
 app.run(debug = True)
--- dbhelpers.py
+++ dbhelpers.py
@@ -37,6 +37,8 @@ def execute_statement(cursor, statement, args=[]):
         results = cursor.fetchall()
         return results
     except mariadb.ProgrammingError as e:
+        if "doesn't have a result set" in str(e):
+            return None
         print("Syntax error in your SQL statement:", e)
         return str(e)
     except mariadb.IntegrityError as e:
@@ -49,7 +51,7 @@ def execute_statement(cursor, statement, args=[]):
         print("Unexpected error", e)
         return str(e)
 
-def run_statement(statement : str, args={}):
+def run_statement(statement : str, args=[]):
     """
     This function expects a valid SQL statement and an optional list of arguments. It connect to the DB,
     executes the statement and closes the connection.
@@ -63,6 +65,7 @@ def run_statement(statement : str, args={}):
     if (cursor == None):
         print("Failed to connect to the DB, statement will not run")
         return None
-    result = execute_statement(cursor, statement, args=[])
+    result = execute_statement(cursor, statement, args)
     disconnect_db(cursor)
     return result
+
